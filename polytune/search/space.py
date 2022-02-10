9aebeedc31b501b4ad9086255bd2701aac87337a
Konstantin Templin <1qnbhd@gmail.com>
2022-02-10 22:58:21 +0300

    cleanup & update

--- polytune/search/space.py
+++ polytune/search/space.py
@@ -1,48 +1,26 @@
-# MIT License
-# 
-# Copyright (c) 2021 Templin Konstantin
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 from pprint import pformat
-from typing import List
+from typing import Union, List
 
 import yaml
 
 from polytune.search import ParameterFactory
-from polytune.search.parameters import Parameter
+from polytune.search.parameters import Categorical, Real, Parameter
 
 
 class SearchSpace:
-
     def __init__(self):
         self.params: List[Parameter] = list()
         self.name2param = dict()
 
-    def add_parameter(self, p: Parameter):
+    def add_parameter(self, p: Parameter) -> None:
         self.params.append(p)
         self.name2param[p.name] = p
 
-    def get_by_name(self, name):
+    def get_by_name(self, name: str) -> Union[Real, Categorical]:
         return self.name2param[name]
 
     def __repr__(self) -> str:
-        representation = pformat(self.params).replace('\n', '\n\t')
+        representation = pformat(self.params).replace("\n", "\n\t")
         return f"SearchSpace(\n\t{representation}\n)"
 
 
@@ -57,4 +35,3 @@ def from_yaml(yaml_file: str) -> SearchSpace:
         space.add_parameter(loaded)
 
     return space
-
--- polytune/storages/memory.py
+++ polytune/storages/memory.py
@@ -1,18 +1,19 @@
 from polytune.storages import Storage
+from numpy import float64
+from polytune.models.configuration import Configuration
 
 
 class MemoryStorage(Storage):
-
     def __init__(self):
         super().__init__()
         self.results = dict()
         self.best_cfg = None
         self.hashes = set()
 
-    def insert_hash(self, h):
+    def insert_hash(self, h: str) -> None:
         self.hashes.add(h)
 
-    def insert_result(self, cfg, result):
+    def insert_result(self, cfg: Configuration, result: float64) -> bool:
         self.results[cfg] = result
 
         if self.best_cfg is None:
@@ -25,11 +26,11 @@ class MemoryStorage(Storage):
             self.best_cfg = cfg
             return True
 
-    def hash_is_exists(self, h):
+    def hash_is_exists(self, h: str) -> bool:
         return h in self.hashes
 
-    def best_configuration(self):
+    def best_configuration(self) -> Configuration:
         return self.best_cfg
 
-    def get_result(self, cfg):
+    def get_result(self, cfg: Configuration) -> float64:
         return self.results.get(cfg, None)
